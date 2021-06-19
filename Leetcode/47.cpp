310a5f33f8041e094c1281b7ea7492f107b04120
Steven <steven84059@gmail.com>
2021-06-19 22:39:36 +0800

    47 update

--- Leetcode/47.cpp
+++ Leetcode/47.cpp
@@ -30,3 +30,41 @@ public:
         return ans;
     }
 };
+
+
+// Version 2 without using set
+class Solution {
+private: 
+    vector<int> tmp;
+    vector<bool> seen;
+    vector<vector<int>> ans;
+    void dfs(vector<int>& nums, vector<vector<int>>& ans) {
+        if(tmp.size() == nums.size()) {
+            ans.push_back(vector<int>(tmp));
+            
+            return;
+        }
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(seen[i] == false) {
+                seen[i] = true;
+                tmp.push_back(nums[i]);
+                dfs(nums, ans);
+                tmp.pop_back();
+                seen[i] = false;
+                
+                while(i + 1 < nums.size() && nums[i] == nums[i + 1])
+                    i++;
+            }
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        seen = vector<bool>(nums.size(), false);
+        
+        dfs(nums, ans);
+        
+        return ans;
+    }
+};
