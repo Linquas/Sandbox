f151ff2b0b98b0ceaa7b3f550d4732671f9ed092
Steven Lin <steven84059@gmail.com>
2021-06-26 00:43:45 -0700

    3-two pointer

--- Leetcode/3.cpp
+++ Leetcode/3.cpp
@@ -31,3 +31,44 @@ public:
         return maxLength;
     }
 };
+
+// Two pointer
+class Solution {
+public:
+    
+    // pwwkew
+    //   L       
+    // R      
+    
+    // count = L-R
+    // R move to R + 1
+    // L move to R
+    // repeat
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0) {
+            return 0;
+        }
+        int l = 0;
+        int r = 0;
+        int longest = -1;
+        unordered_set<int> record;
+        while (l < s.size()) {
+            if (record.find(s[l]) != record.end()) {
+                // found repeating char
+                int count = l - r;
+                longest = max(count, longest);
+                r++;
+                l = r;
+                record.clear();
+            } else {
+                record.insert(s[l]);
+                l++;
+            }
+        }
+        
+        //For last
+        longest = max((int)s.size()-r, longest);
+        
+        return longest;
+    }
+};
